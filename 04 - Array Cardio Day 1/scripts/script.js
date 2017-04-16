37eaf45038fb2e529878599fd1392d4a248672bb
Stephan Griesel <sgriesel@gmail.com>
2017-04-16 07:42:29 +0200

    sort array with sort()

--- 04 - Array Cardio Day 1/scripts/script.js
+++ 04 - Array Cardio Day 1/scripts/script.js
@@ -24,17 +24,30 @@
             return true; //keep it
         }
     });
-    console.log(fifteen);
-    // console.table(fifteen); // to see it in table
+    // console.log(fifteen);
+    console.table(fifteen); // to see it in table
 
     // Array.prototype.map()
     // 2. Give us an array of the inventors' first and last names
     const fullNames = inventors.map(inventor => inventor.first + " " + inventor.last);
-    console.log(fullNames);
+    // console.log(fullNames);
 
 
     // Array.prototype.sort()
     // 3. Sort the inventors by birthdate, oldest to youngest
+
+    /*const ordered = inventors.sort(function(a, b){
+        if(a.year > b.year) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+
+    console.table(ordered);*/
+
+    const ordered = inventors.sort((a,b) => a.year > b.year ? 1 : -1);
+    console.table(ordered);
 
     // Array.prototype.reduce()
     // 4. How many years did all the inventors live?
